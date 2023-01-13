8e2dc025eb218d34083d51addc2185af70b2ccba
Nikitchuk Dmytro <90049427+DimasNDA@users.noreply.github.com>
2023-01-13 12:47:06 +0200

    Add files via upload

---
+++ js/main.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.toogle-mobile-menu').click(function (event) {
+    $('.top-menu').toggleClass('active');
+  });
+  $('.service-item-title').click(function () {
+    $('.service-item-service').removeClass('active');
+    $(this).closest('.service-item-service').addClass('active');
+  });
+  $('.basic-slider').slick({
+    dots: true
+  });
+});
